9283cfbb3edf7500902a0c993d02f4f5970e556c
Annabelle Jiang <annlin@email.unc.edu>
2026-03-25 01:04:42 -0400

    Remove iframe-blocking dev headers

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,3 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    {
-      name: "cross-origin-isolation-headers",
-      configureServer: (server) => {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-          next();
-        });
-      },
-    },
-  ],
-});
+export default defineConfig({});
